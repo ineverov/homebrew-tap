4cce7d30e416ac48d0c5c3097802058fe7915ddb
Ivan Neverov <ivan.neverov@gmail.com>
2022-09-20 22:44:38 -0500

    Maestro 1.6.0

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -2,8 +2,8 @@
 class Maestro < Formula
   desc "Maestro CLI"
   homepage "https://maestro.mobile.dev"
-  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.7.2/maestro-1.7.2.zip"
-  version "1.7.2"
+  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.6.0/maestro-1.6.0.zip"
+  version "1.6.0"
   sha256 "489236d8cb264312bf546c18f6b6a01648c95f032548a6984819870ce758b99b"
   license "Apache-2.0"
 
